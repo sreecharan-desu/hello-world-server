f189f2f86a2eedc22507e2a56957ecd0d2cd96f2
SreeCharan Desu <sreecharan309@gmail.com>
2025-06-02 14:52:11 +0530

    fix : proper linting

--- index.ts
+++ index.ts
@@ -2,15 +2,11 @@ import express from "express";
 import cors from "cors"
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(_req,res)=>{
+app.get('/', (_req, res) => {
     res.send("Hello wolrd");
 })
 
-
-app.listen(3000,()=>{
-    console.log("Listening on localhost:3000");
-});
+app.listen(3000, () => { console.log("Listening on localhost:3000"); });
