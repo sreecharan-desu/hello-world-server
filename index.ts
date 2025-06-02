ed1c37e84fb3685dd88381a5dce4e7d49839ead8
SreeCharan Desu <sreecharan309@gmail.com>
2025-06-02 14:50:08 +0530

    transformation from JS to TS

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-const express = require('express');
-const cors = require("cors");
+import express from "express";
+import cors from "cors"
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
+app.get('/',(_req,res)=>{
     res.send("Hello wolrd");
 })
 
 
-app.listen(3000,(req,res)=>{
+app.listen(3000,()=>{
     console.log("Listening on port : 3000");
 });
