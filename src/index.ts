fb38cf5e374d6789efbab5753969c3e9392b455a
SreeCharan Desu <sreecharan309@gmail.com>
2025-06-06 14:48:21 +0530

    adding volumes and networks

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
 import express from "express";
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (_req, res) => {
+const connect = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017");
+        console.log("Connected to MongoDB");
+    } catch (e) {
+        console.error("Failed to connect to MongoDB", e);
+    }
+};
+
+connect();
+
+app.get("/", (_req, res) => {
     res.send(`Hello ${process.env.SREE}`);
-})
+});
 
-app.listen(3000, () => { console.log("Listening on localhost:3000"); });
-
-
-
-
-
- 
+app.listen(3001, () => {
+    console.log("Listening on http://localhost:3001");
+});
